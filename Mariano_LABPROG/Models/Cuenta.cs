fb83723cd156fd6413088bcb100a749fa4992937
suarezbrian <suarezbrianalan@gmail.com>
2023-04-03 21:47:56 -0300

    Add project files.

---
+++ Mariano_LABPROG/Models/Cuenta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mariano_LABPROG.Models
+{
+    internal class Cuenta
+    {
+        private int titular;
+        private int cantidad;
+
+        public Cuenta(int titular, int cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        public int getTitular() {  return this.titular; }
+        public int getCantidad() { return this.cantidad; }
+        
+        public void Mostrar()
+        {
+            Console.WriteLine("\nEl titular {0}, cuenta {1}", this.titular, this.cantidad);
+        }
+
+        public void Ingresar(int cantidad)
+        {
+            if(cantidad > 0)
+            {
+                this.cantidad += cantidad;
+            }
+        }
+
+        public void Retirar(int cantidad)
+        {
+              this.cantidad -= cantidad;                          
+        }
+    }
+}
---
+++ Mariano_LABPROG/Program.cs
@@ -0,0 +1,70 @@
+﻿using Mariano_LABPROG.Models;
+
+internal class Program
+{
+    /*
+        Consigna
+        Crear una aplicación de consola y una biblioteca de clases que contenga la clase Cuenta.
+
+        Deberá tener los atributos:
+
+        -titular que contendrá la razón social del titular de la cuenta.
+        -cantidad que será un número decimal que representa al monto actual de dinero en la cuenta.
+
+        Construir los siguientes métodos para la clase:
+
+        Un constructor que permita inicializar todos los atributos.
+        Un método getter para cada atributo.
+        mostrar retornará una cadena de texto con todos los datos de la cuenta.
+        ingresar recibirá un monto para acreditar a la cuenta. Si el monto ingresado es negativo, no se hará nada.
+        retirar recibirá un monto para debitar de la cuenta. La cuenta puede quedar en negativo.
+        En el método Main, simular depósitos y extracciones de dinero de la cuenta, e ir mostrando como va variando el saldo.
+    */
+
+    private static void Main(string[] args)
+    {
+        bool seguir = true;
+        int numero = 0;
+        int dinero = 0;
+        Cuenta cuenta = new Cuenta(1000, 2000);
+
+        
+
+        do
+        {
+            cuenta.Mostrar();
+
+            Console.Write("Ingrese un numero:\n1-Ingresar Dinero\n2-Sacar Dinero : ");
+            numero = int.Parse(Console.ReadLine());
+
+            if(numero == 1)
+            {
+                Console.Write("Ingrese la suma de dinero a ingresar : ");
+                dinero = int.Parse(Console.ReadLine());
+                cuenta.Ingresar(dinero);
+            }
+            else
+            {
+                Console.Write("Ingrese la suma de dinero a retirar : ");
+                dinero = int.Parse(Console.ReadLine());
+                cuenta.Retirar(dinero);
+
+            }
+
+
+            Console.WriteLine("Desea seguir ? 1-Si/2-No");
+            numero = int.Parse(Console.ReadLine());
+
+            if(numero == 2)
+            {
+                seguir = false;
+            }
+
+            dinero = 0;
+            Console.Clear();
+
+        } while (seguir);
+
+        Console.ReadKey();
+    }
+}
